a3338759b3cd8d29d8359dc0d74569445b4c8088
gdhehee <ca1lender@hotmail.com>
2026-07-24 21:46:38 +0530

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,9 +1,6 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const fs = require('fs');
-const os = require('os');
 const config = require('../config.json');
 const CatLoggr = require('cat-loggr');
+const users = require('../utils/users');
 
 const log = new CatLoggr();
 
@@ -33,47 +30,9 @@ module.exports = {
 		const account = interaction.options.getString('account');
 		const type = interaction.options.getString('type');
 
-		if (!interaction.member.permissions.has('MANAGE_CHANNELS')) {
-			const errorEmbed = new MessageEmbed()
-				.setColor(config.color.red)
-				.setTitle('You Don\'t Have Permissions!')
-				.setDescription('🛑 Only Admin Can Do It HEHE')
-				.setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-				.setTimestamp();
-			return interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+		if (!users.isAuthorized(interaction.user.id)) {
+			return interaction.reply({ content: '❌ You are not authorized to use this bot.', ephemeral: true });
 		}
 
-		if (!service || !account || (type !== 'free' && type !== 'premium')) {
-			const missingParamsEmbed = new MessageEmbed()
-				.setColor(config.color.red)
-				.setTitle('Missing parameters or invalid type!')
-				.setDescription('You need to specify a service, an account, and a valid type (free or premium)!')
-				.setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-				.setTimestamp();
-			return interaction.reply({ embeds: [missingParamsEmbed], ephemeral: true });
-		}
-
-		let filePath;
-		if (type === 'free') {
-			filePath = `${__dirname}/../free/${service}.txt`;
-		} else if (type === 'premium') {
-			filePath = `${__dirname}/../premium/${service}.txt`;
-		}
-
-		fs.appendFile(filePath, `${os.EOL}${account}`, function (error) {
-			if (error) {
-				log.error(error);
-				return interaction.reply('An error occurred while adding the account.');
-			}
-
-			const successEmbed = new MessageEmbed()
-				.setColor(config.color.green)
-				.setTitle('Account added!')
-				.setDescription(`Successfully added \`${account}\` account to \`${service}\` service **${type}**.`)
-				.setFooter(interaction.user.tag, interaction.user.displayAvatarURL())
-				.setTimestamp();
-
-			interaction.reply({ embeds: [successEmbed], ephemeral: true });
-		});
-	},
-};
+		// Owner bypasses the Discord admin permission check; everyone else still needs MANAGE_CHANNELS.
+		if (!users.isOwner(interaction.user.id) && !interaction.member.permissions.has('MANAGE_CHANNELS')) {
